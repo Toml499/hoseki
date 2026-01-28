ef6bd661270bbf77a094023ad03aa2134292211e
Thomas Végh <thomas.vegh@tugraz.at>
2026-01-29 00:08:27 +0100

    setup for github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/hoseki' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/hoseki/' : '',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'cdn.shopify.com'],
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
